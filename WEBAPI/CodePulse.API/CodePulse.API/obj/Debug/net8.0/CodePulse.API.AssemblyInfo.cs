031c20e31276ed3678a55402b8558c976d50a450
Ramya <ramyayadav2016@gmail.com>
2024-01-11 22:57:53 +0530

    nuget packages installation

--- WEBAPI/CodePulse.API/CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
+++ WEBAPI/CodePulse.API/CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f81683514c15a4e64e685582a5a95364370a81e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5e59140eda43049562c29d8ce912004bff4c96")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
