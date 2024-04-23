45e7b2ad880189bb2cef352b3a3eed80c81055d1
Ramya <ramyayadav2016@gmail.com>
2024-04-23 23:37:38 +0530

    integrated api in ui code

--- UI/CodePulse/src/app/features/category/services/category.service.ts
+++ UI/CodePulse/src/app/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,11 @@ export class CategoryService {
   addCategory(model: AddCategoryRequest) : Observable<void>
   {
      return this.http.post<void>('https://localhost:44301/api/categories', model)
+  }
+
+  //as it is a get method, reuqest object is not necessary it just returns the 
+  //observable of type category list
+  getAllCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>('https://localhost:44301/api/categories');
   }
 }
